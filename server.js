53b18a3d4e620ca12254e6c681f2023a5f3c9d41
diegosiqueirasoares <diego_siqueira@outlook.com.br>
2026-03-25 23:49:54 -0300

    adicionado rotas PUT e DELETE

--- server.js
+++ server.js
@@ -27,3 +27,57 @@ app.get('/incidentes', async (req, res) => {
     const listaIncidentes = await db.all(`SELECT * FROM incidentes`)
     res.json(listaIncidentes)
 })
+
+
+app.get('/incidentes/:id', async (req, res) =>{
+    const { id } = req.params
+
+    const db = await criarBanco() 
+    
+    const incidenteEspecifico = await db.all(`SELECT * FROM incidentes WHERE id = ?`, [id])
+    res.json(incidenteEspecifico)
+})
+
+
+app.post('/incidentes', async(req, res) => {
+    const {tipo_problema, localizacao, descricao, prioridade, nome_solicitante, contato_solicitante, data_registro, hora_registro, imagem_problema} = req.body
+
+const db = await criarBanco()
+
+await db.run(`INSERT INTO incidentes (tipo_problema, localizacao, descricao, prioridade, nome_solicitante, contato_solicitante, data_registro, hora_registro, imagem_problema) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`, [tipo_problema, localizacao, descricao, prioridade, nome_solicitante, contato_solicitante, data_registro, hora_registro, imagem_problema] )
+
+
+res.send(`Incidente novo registrado: ${tipo_problema} registrado na data ${data_registro} por $ {none_solicitante}`)
+})
+
+
+
+app.put('/incidentes/:id', async (req, res) => {
+    
+    const { id } = req.params 
+    
+    const { prioridade, descricao, status_resolucao } = req.body 
+    
+    const db = await criarBanco()
+    
+    await db.run(
+        `UPDATE incidentes 
+         SET prioridade = ?, descricao = ?, status_resolucao = ?  
+         WHERE id = ?`, 
+        [prioridade, descricao, status_resolucao, id]
+    )
+    
+    res.send(`Incidente ${id} atualizado com sucesso!`)
+})
+
+
+app.delete('/incidentes/:id', async (req, res) => {
+    
+    const { id } = req.params
+    
+    const db = await criarBanco()
+   
+    await db.run(`DELETE FROM incidentes WHERE id = ?`, [id])
+   
+    res.send(`O incidente do id ${id} foi removido com sucesso!`) 
+})
